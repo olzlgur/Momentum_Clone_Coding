787d6d3dc8a44390fce9cb3d46edc5d15afaa85a
Lee Ji Heok <olzlgur@naver.com>
2021-07-25 23:23:00 +0900

    2021-07-25

--- Momentum/app.js
+++ Momentum/app.js
@@ -1,1 +1,25 @@
-alert("hi"); 
+const a = 5;
+const b = 2;
+let myName = "JiHyeok";
+
+
+console.log(a+b);
+console.log(a*b);
+console.log(a/b);
+console.log("hello " + myName);
+
+myName = "Hyeok"
+
+console.log("your new name is " + myName);
+
+const daysOfWeek = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
+
+daysOfWeek.push("mon");
+
+const player = {
+  name : "JiHyeok",
+  points : 10,
+  fat : true,
+};
+
+console.log(player.name);
