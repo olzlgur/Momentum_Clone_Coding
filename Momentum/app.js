33112290a8189c5b67da8893e368f2fe745429de
Lee Ji Heok <olzlgur@naver.com>
2021-07-28 23:39:48 +0900

    2021-07-28

--- Momentum/app.js
+++ Momentum/app.js
@@ -1,37 +1,8 @@
-const a = 5;
-const b = 2;
-let myName = "JiHyeok";
-
-
-console.log(a+b);
-console.log(a*b);
-console.log(a/b);
-console.log("hello " + myName);
-
-myName = "Hyeok"
-
-console.log("your new name is " + myName);
-
-const daysOfWeek = ["mon", "tue", "wed", "thu", "fri", "sat", "sun"];
-
-daysOfWeek.push("mon");
-
-const player = {
-  name : "JiHyeok",
-  points : 10,
-  fat : true,
-};
-
-console.log(player.name);
-
-function sayHello(nameOfPerson, age){
-  console.log("Hello my name is " + nameOfPerson + " and I am " + age);
+const age = 96;
+function calculateKrAge(ageOfForeigner){
+  return ageOfForeigner + 2;
 }
 
-sayHello("JiHyeok", 24);
+const krAge = calculateKrAge(age);
 
-function plus(firstNumber, secondNumber){
-  console.log(firstNumber + secondNumber);
-}
-
-plus(8, 60);
+console.log(krAge);
