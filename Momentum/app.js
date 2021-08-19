00a426efe1057550c4c75a7243fc281d5082381c
Lee Ji Heok <olzlgur@naver.com>
2021-08-19 23:36:52 +0900

    EventListener 작성

--- Momentum/app.js
+++ Momentum/app.js
@@ -1,11 +1,20 @@
 const title = document.querySelector(".hello:first-child h1");
 
  function handleTitleClick(){
-   console.log("title was clicked!");
+  title.style.color="red";
  }
 
+ function handleMouseEnter(){
+  title.innerText="Mouse is here!";
+ }
+
+ function handleMouseLeave(){
+  title.innerText="Mouse is gone!";
+ }
 console.dir(title);
 
 title.style.color="gray";
 
 title.addEventListener("click", handleTitleClick);
+title.addEventListener("mouseenter",handleMouseEnter);
+title.addEventListener("mouseleave", handleMouseLeave);
