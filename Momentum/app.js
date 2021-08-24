044fe1908c3496811cca39c3ed9fccda36a786d9
Lee Ji Heok <olzlgur@naver.com>
2021-08-24 23:58:08 +0900

    classList

--- Momentum/app.js
+++ Momentum/app.js
@@ -1,16 +1,16 @@
 const h1 = document.querySelector(".hello:first-child h1");
 
- function handleTitleClick(){
-    const currentColor = h1.style.color;
-    let newColor;
-  if(currentColor === "blue"){
-     newColor = "tomato";
-  }
-  else{
-     newColor="blue";
-  }
-  h1.style.color=newColor;
- }
+
+function handleTitleClick(){
+   const clickedClass = "clicked"
+   if(h1.classList.contains(clickedClass))
+   {
+      h1.classList.remove(clickedClass);
+   }
+   else{
+      h1.classList.add(clickedClass);
+   }
+}
 
 h1.addEventListener("click", handleTitleClick);
 
