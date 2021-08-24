0148fe0b399fad3aab2d595d72860c2fa25e0452
Lee Ji Heok <olzlgur@naver.com>
2021-08-24 23:43:29 +0900

    if-else

--- Momentum/app.js
+++ Momentum/app.js
@@ -1,12 +1,15 @@
 const h1 = document.querySelector(".hello:first-child h1");
 
  function handleTitleClick(){
-  if(h1.style.color === "blue"){
-     h1.style.color = "tomato";
+    const currentColor = h1.style.color;
+    let newColor;
+  if(currentColor === "blue"){
+     newColor = "tomato";
   }
   else{
-     h1.style.color="blue";
+     newColor="blue";
   }
+  h1.style.color=newColor;
  }
 
 h1.addEventListener("click", handleTitleClick);
