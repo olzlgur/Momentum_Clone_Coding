2f547cb58243d36b6da366e3623a6c28a589f765
Lee Ji Heok <olzlgur@naver.com>
2021-08-24 23:39:27 +0900

    if-else

--- Momentum/app.js
+++ Momentum/app.js
@@ -1,43 +1,13 @@
 const h1 = document.querySelector(".hello:first-child h1");
 
  function handleTitleClick(){
-  h1.style.color="red";
+  if(h1.style.color === "blue"){
+     h1.style.color = "tomato";
+  }
+  else{
+     h1.style.color="blue";
+  }
  }
 
- function handleMouseEnter(){
-  h1.innerText="Mouse is here!";
- }
+h1.addEventListener("click", handleTitleClick);
 
- function handleMouseLeave(){
-  h1.innerText="Mouse is gone!";
- }
-
- function handleWindowResize(){
-    document.body.style.backgroundColor = "tomato";
- }
-
- function handleWindowCopy(){
-    alert("copier!");
- }
-
- function handleWindowOffLine(){
-    alert("SOS no WIFI");
- }
-
- function handleWindowOnLine(){
-    alert("OnLine");
- }
-console.dir(h1);
-
-
-h1.style.color="gray";
-
-h1.onclick = handleTitleClick;
-h1.onmouseenter = handleMouseEnter;
-h1.onmouseleave = handleMouseLeave;
-
-
-window.addEventListener("resize", handleWindowResize);
-window.addEventListener("copy", handleWindowCopy);
-window.addEventListener("offLine", handleWindowOffLine);
-window.addEventListener("onLine", handleWindowOnLine);
