8456c570b5a892ee6ab4003275d2cc339a99fe38
Lee Ji Heok <olzlgur@naver.com>
2021-08-24 23:34:06 +0900

    window 함수 기능

--- Momentum/app.js
+++ Momentum/app.js
@@ -1,27 +1,43 @@
-const title = document.querySelector(".hello:first-child h1");
+const h1 = document.querySelector(".hello:first-child h1");
 
  function handleTitleClick(){
-  title.style.color="red";
+  h1.style.color="red";
  }
 
  function handleMouseEnter(){
-  title.innerText="Mouse is here!";
+  h1.innerText="Mouse is here!";
  }
 
  function handleMouseLeave(){
-  title.innerText="Mouse is gone!";
+  h1.innerText="Mouse is gone!";
  }
 
  function handleWindowResize(){
     document.body.style.backgroundColor = "tomato";
  }
 
-console.dir(title);
+ function handleWindowCopy(){
+    alert("copier!");
+ }
 
-title.style.color="gray";
+ function handleWindowOffLine(){
+    alert("SOS no WIFI");
+ }
 
-title.onclick = handleTitleClick;
-title.onmouseenter = handleMouseEnter;
-title.onmouseleave = handleMouseLeave;
+ function handleWindowOnLine(){
+    alert("OnLine");
+ }
+console.dir(h1);
+
+
+h1.style.color="gray";
+
+h1.onclick = handleTitleClick;
+h1.onmouseenter = handleMouseEnter;
+h1.onmouseleave = handleMouseLeave;
+
 
 window.addEventListener("resize", handleWindowResize);
+window.addEventListener("copy", handleWindowCopy);
+window.addEventListener("offLine", handleWindowOffLine);
+window.addEventListener("onLine", handleWindowOnLine);
