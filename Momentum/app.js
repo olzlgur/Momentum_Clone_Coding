edb27b0101d47069bfc42a73d4e65214275f94d4
Lee Ji Heok <olzlgur@naver.com>
2021-08-27 23:23:27 +0900

    로그인 버튼 추가

--- Momentum/app.js
+++ Momentum/app.js
@@ -1,10 +1,9 @@
-const h1 = document.querySelector(".hello:first-child h1");
+const loginForm = document.querySelector(".login-form");
+const loginInput = loginForm.querySelector("input");
+const loginButton = loginForm.querySelector("button");
 
-
-function handleTitleClick(){
-   const clickedClass = "clicked"
-   h1.classList.toggle(clickedClass);
+function LoginBtnClick(){
+   console.log("Hello" , loginInput.value);
 }
 
-h1.addEventListener("click", handleTitleClick);
-
+loginButton.addEventListener("click",LoginBtnClick);
